70787ae2923d1b40eb01c5ce5e7e8791af18e7e0
wild_fat <artem.vod@gmail.com>
2023-04-29 14:01:31 +0300

    0.1
    init

---
+++ Source/LD53/LD53GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "LD53GameModeBase.h"
+
---
+++ Source/LD53/LD53GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LD53GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LD53_API ALD53GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
